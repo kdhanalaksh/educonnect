4b439841b0f95f03bdccf389c13ce992927eee70
dhanalakshmik <kdhanalakshmi2005@gmail.com>
2025-07-11 16:09:48 +0530

    Initial Commit

---
+++ app/src/main/java/com/saveetha/educonnect/LoginPageActivity.kt
@@ -0,0 +1,20 @@
+package com.saveetha.educonnect
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.login_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
